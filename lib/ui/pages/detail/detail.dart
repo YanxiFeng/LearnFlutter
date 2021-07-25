878c08b406a84a46d50bec28ab3676513b57f1f7
Yvan <ios.sysail@gmail.com>
2021-07-25 12:22:54 +0800

    detail add list view

--- lib/ui/pages/detail/detail.dart
+++ lib/ui/pages/detail/detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learnflutter/core/model/meal_model.dart';
+import 'package:learnflutter/ui/pages/detail/detail_content.dart';
 
 class HYDetailScreen extends StatelessWidget {
   static const String routeName = "/detail";
@@ -11,9 +12,7 @@ class HYDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Text(meal.title),
-      ),
+      body: HYDetailContent(meal),
     );
   }
 }
---
+++ lib/ui/pages/detail/detail_content.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:learnflutter/core/model/meal_model.dart';
+
+class HYDetailContent extends StatelessWidget {
+  final HyMealModel _meal;
+  HYDetailContent(this._meal);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          buildMakeImage(),
+          buildMakeTitle(context, "制作材料"),
+          buildMakeMaterial(context),
+          buildMakeTitle(context, "制作步骤"),
+          buildMakeSteps(context)
+        ],
+      ),
+    );
+  }
+
+  // 图片
+  Widget buildMakeImage() {
+    return Container(
+      child: Image.network(_meal.imageUrl),
+    );
+  }
+
+  // 公共方法-内容框
+  Widget buildMakeContent(
+      {required BuildContext context, required Widget child}) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: MediaQuery.of(context).size.width - 30,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.all(Radius.circular(6))),
+      child: child,
+    );
+  }
+
+  // 制作材料
+  Widget buildMakeMaterial(BuildContext context) {
+    return buildMakeContent(
+        context: context,
+        child: ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: _meal.ingredients.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                color: Colors.amber,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Text(_meal.ingredients[index]),
+                ),
+              );
+            }));
+  }
+
+  // 公共方法-标题
+  Widget buildMakeTitle(BuildContext context, String title) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .display3!
+            .copyWith(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  // 制作步骤
+  Widget buildMakeSteps(BuildContext context) {
+    return buildMakeContent(
+        context: context,
+        child: ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: _meal.steps.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Text(_meal.steps[index]),
+                ),
+              );
+            }));
+  }
+}
