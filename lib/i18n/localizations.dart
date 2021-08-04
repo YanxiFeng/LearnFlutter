d546e26030513f7e54a8f065fba4d79f9f4d6056
Yvan <ios.sysail@gmail.com>
2021-08-04 23:51:54 +0800

    flutter intl

--- lib/i18n/localizations.dart
+++ lib/i18n/localizations.dart
@@ -6,6 +6,8 @@ class HYLocalizations {
   final Locale locale;
   HYLocalizations(this.locale);
 
+  Map<String, Map<String, String>> _localizeValues = {};
+
   Future loadJson() async {
     // 1.加载json文件
     final jsonString = await rootBundle.loadString("assets/json/i18n.json");
@@ -17,11 +19,6 @@ class HYLocalizations {
       return MapEntry(key, value.cast<String, String>());
     });
   }
-
-  static Map<String, Map<String, String>> _localizeValues = {
-    "en": {"title": "Home", "hello": "Hello", "pickTime": "Pick a time~"},
-    "zh": {"title": "首页", "hello": "您好", "pickTime": "选择一个时间~"},
-  };
 
   String? get title {
     return _localizeValues[locale.languageCode]!["title"];
